3f45a271c372ced17ee14461725541b1c9bb2ed5
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-10 21:40:32 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,8 +2,8 @@ class Cat
   attr_accessor :mood
   attr_reader :name
 
-  def initialize(name)
+  def initialize(name, mood = "Nervous")
     @name = name
-    @mood = "Nervous"
+    @mood = mood
   end
 end
